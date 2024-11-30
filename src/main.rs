a3f638fe147b6bd68f272621c9d1b6f2776a8972
Zach <zacharyeverett.00@gmail.com>
2024-11-30 23:56:52 +1100

    very cool looking basic one ball + one light simulation

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ mod objects;
 const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
 const FOV: f32 = 90.0 * f32::consts::PI / 180.0; // 90 degrees
+const SAMPLES_PER_PIXEL: usize = 2;
+const MAX_BOUNCES: usize = 100;
 fn main() {
     let mut image = png_manager::Image::new(WIDTH as u32, HEIGHT as u32);
     let scene = scene_manager::Scene::new("scenes/scene_1.json".to_string());
@@ -21,7 +23,7 @@ fn main() {
             let dir_y = -y_angle.sin();
             let length = (dir_x.powi(2) + dir_y.powi(2) + 1.0).sqrt();
             let mut ray = objects::Ray::new([0.0, 0.0, -100.0], [dir_x / length, dir_y / length, 1.0 / length]);
-            let color = scene.trace(&mut ray, 1, 1);
+            let color = scene.trace(&mut ray, MAX_BOUNCES, SAMPLES_PER_PIXEL);
             image.set_pixel(x as u32, y as u32, [color[0], color[1], color[2], 255]);
         }
     }
--- src/scene_manager.rs
+++ src/scene_manager.rs
@@ -1,11 +1,18 @@
 use std::fs::File;
 use serde_json::{Result, Value};
 use super::objects::{Sphere, Ray, Hit};
-//use rand::Rng;
+use rand::prelude::*;
 pub struct Scene {
     pub spheres: Vec<Sphere>,
 }
-
+//sd 1, mean 0
+fn gaussian_random(rng: &mut ThreadRng) -> f32 {
+    let mut sum = 0.0;
+    for _ in 0..12 {
+        sum += rng.gen::<f32>();
+    }
+    sum - 6.0
+}
 impl Scene {
     pub fn new(scene_name: String) -> Scene {
         let mut scene = Scene {
@@ -41,13 +48,18 @@ impl Scene {
     }
     pub fn trace(&self, ray: &mut Ray, bounces: usize, samples: usize) -> [u8; 3] {
         let mut colour_sum = [0.0, 0.0, 0.0];
-        let initial_origin = ray.origin;
-        let initial_direction = ray.direction;
+        let initial_origin = [ray.origin[0], ray.origin[1], ray.origin[2]];
+        let initial_direction = [ray.direction[0], ray.direction[1], ray.direction[2]];
+
+        let mut rng = rand::thread_rng();
+
         for _ in 0..samples {
 
-            ray.origin = initial_origin;
-            ray.direction = initial_direction;
-
+            ray.origin = [initial_origin[0], initial_origin[1], initial_origin[2]];
+            ray.direction = [initial_direction[0], initial_direction[1], initial_direction[2]];
+            ray.color = [1.0, 1.0, 1.0];
+            ray.light = 0.0;
+            let mut commited_color = [0.0, 0.0, 0.0];
             for _ in 0..bounces {
                 let mut closest_hit = Hit::new(f32::INFINITY, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 0.0);
                 for sphere in &self.spheres {
@@ -60,19 +72,48 @@ impl Scene {
                 if closest_hit.t == f32::INFINITY {
                     break;
                 }
+
+                ray.light = closest_hit.light;
+
+                commited_color[0] += ray.color[0] * ray.light;
+                commited_color[1] += ray.color[1] * ray.light;
+                commited_color[2] += ray.color[2] * ray.light;
+
                 ray.color[0] *= closest_hit.color[0] as f32;
                 ray.color[1] *= closest_hit.color[1] as f32;
                 ray.color[2] *= closest_hit.color[2] as f32;
+
                 ray.origin = closest_hit.location;
                 
                 //not right
-                ray.direction = closest_hit.normal;
+                //ray.direction = closest_hit.normal;
 
-                ray.light += closest_hit.light;
+                //mirror reflection
+                /* 
+                let dot = ray.direction[0] * closest_hit.normal[0] +
+                          ray.direction[1] * closest_hit.normal[1] +
+                          ray.direction[2] * closest_hit.normal[2];
+                ray.direction[0] -= 2.0 * dot * closest_hit.normal[0];
+                ray.direction[1] -= 2.0 * dot * closest_hit.normal[1];
+                ray.direction[2] -= 2.0 * dot * closest_hit.normal[2];
+                */
+                //random reflection
+                let x = gaussian_random(&mut rng);
+                let y = gaussian_random(&mut rng);
+                let z = gaussian_random(&mut rng);
+                let length = (x.powi(2) + y.powi(2) + z.powi(2)).sqrt();
+                ray.direction[0] = x / length;
+                ray.direction[1] = y / length;
+                ray.direction[2] = z / length;
+                if ray.direction[0] * closest_hit.normal[0] + ray.direction[1] * closest_hit.normal[1] + ray.direction[2] * closest_hit.normal[2] < 0.0 {
+                    ray.direction[0] = -ray.direction[0];
+                    ray.direction[1] = -ray.direction[1];
+                    ray.direction[2] = -ray.direction[2];
+                }
             }
-            colour_sum[0] += ray.color[0] * ray.light;
-            colour_sum[1] += ray.color[1] * ray.light;
-            colour_sum[2] += ray.color[2] * ray.light;
+            colour_sum[0] += commited_color[0];
+            colour_sum[1] += commited_color[1];
+            colour_sum[2] += commited_color[2];
         }
         [
             (colour_sum[0] * 255.0 / samples as f32) as u8,
